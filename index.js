58be005dc924e9140ad28677be8941cd0cfd3f6a
Brisa Carter <brisa.carter@gmail.com>
2019-05-11 18:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function dec(n){
   return n-=1
 }
 
-function makeInt(n, 10){
-  return parseInt(n)
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n, 10)
 }
